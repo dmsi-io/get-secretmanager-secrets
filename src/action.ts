3338ecd95c94048b6732ee46b7173ed8ae1750d1
Adam Lehechka <ajlehechka@gmail.com>
2022-04-05 11:27:50 -0500

    refactor production ref code

--- src/action.ts
+++ src/action.ts
@@ -1,5 +1,4 @@
 import { getInput } from '@actions/core';
-import { Octokit } from 'octokit';
 
 export function getProjectID(): string {
   return (
@@ -12,11 +11,11 @@ export function getProjectID(): string {
   );
 }
 
-function getRepoOwner(): string {
+export function getRepoOwner(): string {
   return process.env.GITHUB_REPOSITORY_OWNER || '';
 }
 
-function getRepo(): string {
+export function getRepo(): string {
   const fullRepo = process.env.GITHUB_REPOSITORY || '';
   const parts = fullRepo.split('/');
 
@@ -27,66 +26,19 @@ function getRepo(): string {
   return '';
 }
 
-function getSha(): string {
+export function getSha(): string {
   return process.env.GITHUB_SHA || '';
 }
 
-function isProdRef(ref?: string): boolean {
-  if (ref && (ref === 'main' || ref.match(/release\/*/))) {
-    return true;
-  }
-
-  return false;
+export function getGithubToken(): string {
+  return getInput('GHA_ACCESS_TOKEN');
 }
 
-export async function isProductionRef(): Promise<boolean> {
-  const REF_NAME = process.env.GITHUB_REF_NAME;
-  const REF_TYPE = process.env.GITHUB_REF_TYPE;
+export function getRefName() {
+  return process.env.GITHUB_REF_NAME;
+}
 
-  if (REF_TYPE === 'branch' && isProdRef(REF_NAME)) {
-    return true;
-  }
-
-  if (REF_TYPE === 'tag') {
-    const octokit = new Octokit({ auth: getInput('GHA_ACCESS_TOKEN') });
-    const owner = getRepoOwner();
-    const repo = getRepo();
-    const sha = getSha();
-
-    try {
-      const branches = await octokit.request('GET /repos/{owner}/{repo}/branches', {
-        owner,
-        repo,
-      });
-      const branchNames = branches.data.map((branch) => branch.name).filter(isProdRef);
-
-      const commits = await octokit.request('GET /repos/{owner}/{repo}/commits', {
-        owner,
-        repo,
-        sha,
-        per_page: 1,
-      });
-      const commitDate = commits.data[0]?.commit.committer?.date;
-      if (!commitDate) return false;
-
-      for (const branchName of branchNames) {
-        const branchCommits = await octokit.request('GET /repos/{owner}/{repo}/commits', {
-          owner,
-          repo,
-          sha: branchName,
-          per_page: 1,
-          since: commitDate,
-          until: commitDate,
-        });
-        if (branchCommits.data.length === 0) continue;
-        if (branchCommits.data[0].sha === sha) return true;
-      }
-    } catch (error) {
-      return false;
-    }
-
-    return false;
-  }
-
-  return false;
+type RefType = 'branch' | 'tag' | undefined;
+export function getRefType(): RefType {
+  return process.env.GITHUB_REF_TYPE as RefType;
 }
